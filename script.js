f82f25430a8c2e280afbea2a49434f58c391d31e
gerardnico <gerardnico@gmail.com>
2019-12-03 23:17:14 +0100

    Backup consent window

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
 var ads = {
+    selector: '#gdpr_consent',
     consent: function () {
-        let modal_id = '#dialog';
-        jQuery(modal_id).dialog(
-            {
-                autoOpen: true,
-                width: '100%',
-                height: 'auto',
-                draggable: false,
-                resizable: false,
-                // position: { my: "center bottom", at: "bottom", of: window  },
-                dialogClass: 'ads_style bg-light'
-            }
-        );
+        let selector = this.selector;
+        // Show the alert
+        jQuery(selector).addClass('show');
+        // A click on the document, close it
+        jQuery( document ).click(function() {
+            jQuery(selector).alert('close')
+        });
+        // Except in the element itself
+        jQuery( selector ).click(function(e) {
+            e.stopPropagation();
+        });
+        // When it's closed, we save the consent
+        jQuery(selector).on('closed.bs.alert', function () {
+            //  This event is fired when the alert has been closed (will wait for CSS transitions to complete)
+            console.log("Gdpr was closed");
+        })
     }
 };
 
 
-//ads.consent();
+ads.consent();
