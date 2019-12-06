5553cc7bd922cc8ee8f88fd8ce655cb839548cb6
gerardnico <gerardnico@gmail.com>
2019-12-06 04:59:53 +0100

    Yolo !

--- script.js
+++ script.js
@@ -13,16 +13,23 @@ var gdpr = {
         let consent = ( localStorage.getItem(consentKey) === 'true' );
         if (!consent) {
             // Load the consent file
-            jQuery("body").load("consent_modal.html", function () {
-                let selector = '#gdpr_consent';
-                // Show the alert
-                jQuery(selector).addClass('show');
-                // When it's closed, we save the consent
-                jQuery(selector).on('closed.bs.alert', function () {
-                    //  This event is fired when the alert has been closed (will wait for CSS transitions to complete)
-                    localStorage.setItem(consentKey, true);
-                })
-            });
+            consentBox = `
+                <div id="gdpr_consent" class="container alert alert-secondary alert-dismissible fixed-bottom text-center fade" role="alert" >
+                    By using our site, you acknowledge that you have read and understand our <a href="/legal/cookie">Cookie Policy</a>, <a href="/legal/privacy">Privacy Policy</a>, and our <a href="/legal/terms">Terms of Service</a>.
+                    <button type="button" class="close" style="float:initial"  data-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+            `
+            jQuery("body").append(consentBox);
+            let selector = '#gdpr_consent';
+            // Show the alert
+            jQuery(selector).addClass('show');
+            // When it's closed, we save the consent
+            jQuery(selector).on('closed.bs.alert', function () {
+                //  This event is fired when the alert has been closed (will wait for CSS transitions to complete)
+                localStorage.setItem(consentKey, true);
+            })
         }
     }
 };
